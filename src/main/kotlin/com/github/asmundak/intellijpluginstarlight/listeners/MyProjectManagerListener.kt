7d929fb2005a6dfcec9e3ddc3ea1bfd21969893d
GitHub Action <action@github.com>
2020-10-15 17:54:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/asmundak/intellijpluginstarlight/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/asmundak/intellijpluginstarlight/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.asmundak.intellijpluginstarlight.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.asmundak.intellijpluginstarlight.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/asmundak/intellijpluginstarlight/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.asmundak.intellijpluginstarlight.services
+
+import com.github.asmundak.intellijpluginstarlight.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
